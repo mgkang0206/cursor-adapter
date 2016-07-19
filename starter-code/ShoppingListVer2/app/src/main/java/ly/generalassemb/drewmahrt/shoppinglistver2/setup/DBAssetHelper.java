3b7622f759abb8d90dd6504e325511a2b234c7d5
Brad Z <brad@makesnaps.com>
2016-07-19 10:02:33 -0700

    Update DBAssetHelper.java

--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/setup/DBAssetHelper.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/setup/DBAssetHelper.java
@@ -9,10 +9,63 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class DBAssetHelper extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "SHOPPING_DB";
-    private static final int DATABASE_VERSION = 7;
+    private static final String TAG = ShoppingListSQLiteHelper.class.getCanonicalName();
 
-    public DBAssetHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    private static final int DATABASE_VERSION = 8;
+    public static final String DATABASE_NAME = "SHOPPING_DB";
+    public static final String SHOPPING_LIST_TABLE_NAME = "SHOPPING_LIST";
+
+    public static final String COL_ID = "_id";
+    public static final String COL_ITEM_NAME = "ITEM_NAME";
+    public static final String COL_DESC = "DESCRIPTION";
+    public static final String COL_PRICE = "PRICE";
+    public static final String COL_TYPE = "TYPE";
+
+    public static final String[] SHOPPING_COLUMNS = {COL_ID, COL_ITEM_NAME, COL_DESC, COL_PRICE, COL_PRICE, COL_TYPE};
+
+    private static final String CREATE_SHOPPING_LIST_TABLE =
+            "create table " + SHOPPING_LIST_TABLE_NAME + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_ITEM_NAME + " TEXT,"
+                    + COL_DESC + " TEXT," + COL_PRICE + " TEXT," + COL_TYPE + " TEXT);";
+
+
+    private static ShoppingListSQLiteHelper instance;
+
+    public static ShoppingListSQLiteHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new ShoppingListSQLiteHelper(context);
+        }
+        return instance;
+    }
+
+    private ShoppingListSQLiteHelper(Context context) {
+        super(context, "SHOPPING_DB", null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(TAG, CREATE_SHOPPING_LIST_TABLE);
+        db.execSQL(CREATE_SHOPPING_LIST_TABLE);}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + SHOPPING_LIST_TABLE_NAME);
+        this.onCreate(db);
+    }
+
+    public boolean insertShopping(String item, String desc, String price, String type) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ITEM_NAME, item);
+        contentValues.put(COL_DESC, desc);
+        contentValues.put(COL_PRICE, price);
+        contentValues.put(COL_TYPE, type);
+        db.insert(SHOPPING_LIST_TABLE_NAME, null, contentValues);
+        return true;
+    }
+
+    public Cursor getShoppingList(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(SHOPPING_LIST_TABLE_NAME,SHOPPING_COLUMNS,null,null,null,null,null,null);
+        return cursor;
     }
 }
