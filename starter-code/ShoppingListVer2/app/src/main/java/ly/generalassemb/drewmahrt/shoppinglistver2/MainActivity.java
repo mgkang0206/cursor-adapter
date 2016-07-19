e3cb462d546726a6fd69e8c13f98f01210e1aa9d
Rcureton <rashad.cureton@gmail.com>
2016-07-18 20:15:48 -0700

    Fixed missing Shopping List Class

--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -2,25 +2,70 @@ package ly.generalassemb.drewmahrt.shoppinglistver2;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
 
 public class MainActivity extends AppCompatActivity {
+    ListView mList;
+    Cursor mCursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mList= (ListView) findViewById(R.id.shopping_list_view);
         //Ignore the two lines below, they are for setup
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
 
+        ShoppingListSQLiteHelper helper = ShoppingListSQLiteHelper.getInstance(MainActivity.this);
+        mCursor= helper.getShoppingList();
+
+
+        CursorAdapter cursorAdapter = new CursorAdapter(MainActivity.this, mCursor, 0) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(context).inflate(R.layout.shopping_list_items, parent, false);
+
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+
+
+                TextView textView= (TextView)view.findViewById(R.id.textView1);
+                TextView textPrice= (TextView)view.findViewById(R.id.textView2);
+                TextView textDescription= (TextView)view.findViewById(R.id.textView3);
+                TextView textType= (TextView)view.findViewById(R.id.textView4);
+
+                String itemName= cursor.getString(cursor.getColumnIndex("ITEM_NAME"));
+                String itemPrice= cursor.getString(cursor.getColumnIndex("PRICE"));
+                String itemDescription= cursor.getString(cursor.getColumnIndex("DESCRIPTION"));
+                String itemType= cursor.getString(cursor.getColumnIndex("TYPE"));
+
+                textView.setText(itemName);
+                textPrice.setText(itemPrice);
+                textDescription.setText(itemDescription);
+                textType.setText(itemType);
+
+            }
+
+        };mList.setAdapter(cursorAdapter);
+
     }
+
+
+
+
 }
---
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/ShoppingListSQLiteHelper.java
@@ -0,0 +1,58 @@
+package ly.generalassemb.drewmahrt.shoppinglistver2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ra on 7/18/16.
+ */
+public class ShoppingListSQLiteHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = ShoppingListSQLiteHelper.class.getCanonicalName();
+
+    private static final int DATABASE_VERSION = 7;
+    public static final String DATABASE_NAME = "SHOPPING_DB";
+    public static final String SHOPPING_LIST_TABLE_NAME = "SHOPPING_LIST";
+
+    public static final String COL_ID = "_id";
+    public static final String COL_ITEM_NAME = "ITEM_NAME";
+    public static final String COL_DESC = "DESCRIPTION";
+    public static final String COL_PRICE = "PRICE";
+    public static final String COL_TYPE = "TYPE";
+
+    public static final String[] SHOPPING_COLUMNS = {COL_ID, COL_ITEM_NAME, COL_DESC, COL_PRICE, COL_PRICE, COL_TYPE};
+
+    private static final String CREATE_SHOPPING_LIST_TABLE =
+            "CREATE_TABLE" + SHOPPING_LIST_TABLE_NAME + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_ITEM_NAME + "TEXT"
+                    + COL_DESC + "TEXT" + COL_PRICE + "TEXT" + COL_TYPE + "TEXT)";
+
+
+    private static ShoppingListSQLiteHelper instance;
+
+    public static ShoppingListSQLiteHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new ShoppingListSQLiteHelper(context);
+        }
+        return instance;
+    }
+
+    private ShoppingListSQLiteHelper(Context context) {
+        super(context, "SHOPPING_DB", null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {db.execSQL(CREATE_SHOPPING_LIST_TABLE);}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + SHOPPING_LIST_TABLE_NAME);
+        this.onCreate(db);
+    }
+    public Cursor getShoppingList(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(SHOPPING_LIST_TABLE_NAME,SHOPPING_COLUMNS,null,null,null,null,null,null);
+        return cursor;
+    }
+}
